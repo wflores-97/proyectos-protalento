ac8babe378593d970bfd70c770d59c18049182ea
wflores-97 <wilsonflores9719@gmail.com>
2023-09-23 18:45:02 -0600

    primer ejercicio de tarea protalento

---
+++ proyecto1.py
@@ -0,0 +1,13 @@
+entero = 10
+flotante = 11.3
+boleano = True
+cadena = "concatenacion de variables: "
+
+concatenar = cadena, str(entero), str(flotante), str(boleano)
+print(concatenar)
+
+#mediante el uso de una variable de tipo int de Python puede 
+#almacenar números de -231 a 231 – 1, o lo que es lo mismo, de -2.147.483.648 a 2.147.483.647.
+
+#Los números en coma flotante pierden la gran precisión
+#que tienen los números enteros y almacenan solo 16 cifras de precisión.
